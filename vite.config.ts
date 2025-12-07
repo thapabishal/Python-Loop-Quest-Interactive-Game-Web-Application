39cf45a9c48d973eb2927f5953699b10a0fd5dd5
Bishal Khatri <thapabishal1245@gmail.com>
2025-12-08 02:14:49 +0545

    ','

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
     react(), 
-    cloudflare() 
   ],
   server: {
-    
     allowedHosts: true, 
   },
   build: {    
